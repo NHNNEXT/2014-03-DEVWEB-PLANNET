e3e869f0c97df99b13b593cefa1b606595a4774b
sturdyegg <sturdyegg@gmail.com>
2014-12-12 02:43:36 +0900

    메일발쏴

--- PlannetServer/src/net/plannet/login/Mail.java
+++ PlannetServer/src/net/plannet/login/Mail.java
@@ -1,37 +1,66 @@
 package net.plannet.login;
 
-import javax.mail.*;
-import javax.mail.internet.*;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.util.*;
-
 @WebServlet("/login/mail")
-class Mail extends HttpServlet {
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+public class Mail extends HttpServlet {
+
+	private static final String SMTP_HOST = "smtp.gmail.com";
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
-		Mail.sendMail();
-	}
-	public static void sendMail(){
-		Properties props = new Properties();
-		props.put("mail.smtp.com", "smtp.gmail.com");
+
+		String host = "smtp.gmail.com";
+		String from = "plannetnoreply";
+		String pass = "plannet1004";
+		Properties props = System.getProperties();
+		props.put("mail.smtp.starttls.enable", "true"); // added this line
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.user", from);
+		props.put("mail.smtp.password", pass);
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+
+		String[] to = { "sturdyegg@gmail.com" }; // added this line
+
 		Session session = Session.getDefaultInstance(props, null);
-		String to = "sturdyegg@gmail.com";
-		String from = "from@gmail.com";
-		String subject = "Testing...";
-		Message msg = new MimeMessage(session);
+		MimeMessage message = new MimeMessage(session);
 		try {
-			msg.setFrom(new InternetAddress(from));
-			msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			msg.setSubject(subject);
-			msg.setText("Working fine..!");
-		} catch (Exception exc) {
+			message.setFrom(new InternetAddress(from));
+
+			InternetAddress[] toAddress = new InternetAddress[to.length];
+
+			// To get the array of addresses
+			for (int i = 0; i < to.length; i++) { // changed from a while loop
+				toAddress[i] = new InternetAddress(to[i]);
+			}
+			System.out.println(Message.RecipientType.TO);
+
+			for (int i = 0; i < toAddress.length; i++) { // changed from a while
+															// loop
+				message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+			}
+			message.setSubject("sending in a group");
+			message.setText("Welcome to JavaMail");
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host, from, pass);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+		} catch (MessagingException e) {
+			e.printStackTrace();
 		}
 	}
 }
